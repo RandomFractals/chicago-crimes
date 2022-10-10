778d837caf87415ba4dae980e1bcc7baee1bb78a
RandomFractals <RandomFractals@users.noreply.github.com>
2022-10-09 19:46:48 -0500

    finish simple crimes info R script in R Studio (#9)
    and add R studio pic to /docs/images

--- scripts/chicago-crimes-duckdb.r
+++ scripts/chicago-crimes-duckdb.r
@@ -1,9 +1,9 @@
-library("DBI")
+library(DBI)
 
 # create in-memory database
 db <- dbConnect(duckdb::duckdb(), dbdir = ":memory:")
 
-dbExecute(db, "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('../data/crimes.csv')")
+crimes <- dbExecute(db, "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('../data/crimes.csv')")
 
-crimes <- dbGetQuery("DESCRIBE crimes;")
-print(crimes)
+crimes_info <- dbGetQuery(db, "DESCRIBE crimes;")
+print(crimes_info)
