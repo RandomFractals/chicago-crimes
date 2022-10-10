3d2bb83413be5eb328104d760955b391da6182e0
RandomFractals <RandomFractals@users.noreply.github.com>
2022-10-09 19:00:22 -0500

    start simple duckdb r script for now (#9)

---
+++ scripts/chicago-crimes-duckdb.r
@@ -0,0 +1,9 @@
+library("DBI")
+
+# create in-memory database
+db <- dbConnect(duckdb::duckdb(), dbdir = ":memory:")
+
+dbExecute(db, "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('../data/crimes.csv')")
+
+crimes = dbGetQuery("DESCRIBE crimes;")
+print(crimes)
