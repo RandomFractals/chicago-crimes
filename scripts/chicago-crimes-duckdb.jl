c908fa69f2d12c93a21b0383e6a88441c5a21a8c
RandomFractals <RandomFractals@users.noreply.github.com>
2022-10-13 14:31:03 -0500

    refine .jl repl script print statements (#10)

--- scripts/chicago-crimes-duckdb.jl
+++ scripts/chicago-crimes-duckdb.jl
@@ -10,18 +10,20 @@ startTime = Dates.Time(Dates.now())
 DBInterface.execute(db, "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('./data/crimes.csv')")
 endTime = Dates.Time(Dates.now())
 loadTime = round((endTime - startTime), Dates.Second)
-
 println("CSV data loaded in $(loadTime)")
 
+# print crimes db info schema
+println("\nNew DuckDB info schema:")
 dataTables = DBInterface.execute(db, "SELECT * from information_schema.tables;")
 println(dataTables)
 
 # get crimes table info
+println("\ncrimes table info:")
 crimesInfo = DBInterface.execute(db, "DESCRIBE crimes;")
 println(crimesInfo)
 
 # drop crimes data table
-println("Dropping crimes table ...")
+println("\nDropping crimes table ...")
 DBInterface.execute(db, "DROP TABLE crimes;")
 
 println("Empty DuckDB 'information_schema.tablest' schema:")
