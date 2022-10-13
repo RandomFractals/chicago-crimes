06ba1ba056a9044203a32436bd4ca218c59159b3
RandomFractals <RandomFractals@users.noreply.github.com>
2022-10-13 14:16:34 -0500

    add basic Julia duckdb crimes CSV data loading (#10)

---
+++ scripts/chicago-crimes-duckdb.jl
@@ -0,0 +1,29 @@
+using Dates
+using DuckDB
+
+# create a new in-memory database
+db = DBInterface.connect(DuckDB.DB, ":memory:")
+
+# import raw CSV crimes data into in-memory DuckDB instance
+println("Loading CSV data ...")
+startTime = Dates.Time(Dates.now())
+DBInterface.execute(db, "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('./data/crimes.csv')")
+endTime = Dates.Time(Dates.now())
+loadTime = round((endTime - startTime), Dates.Second)
+
+println("CSV data loaded in $(loadTime)")
+
+dataTables = DBInterface.execute(db, "SELECT * from information_schema.tables;")
+println(dataTables)
+
+# get crimes table info
+crimesInfo = DBInterface.execute(db, "DESCRIBE crimes;")
+println(crimesInfo)
+
+# drop crimes data table
+println("Dropping crimes table ...")
+DBInterface.execute(db, "DROP TABLE crimes;")
+
+println("Empty DuckDB 'information_schema.tablest' schema:")
+dataTables = DBInterface.execute(db, "SELECT * from information_schema.tables;")
+println(dataTables)
