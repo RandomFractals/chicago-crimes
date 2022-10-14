5a71aee5f96609d8277f26b22badb79acbb9df43
RandomFractals <RandomFractals@users.noreply.github.com>
2022-10-14 05:24:41 -0500

    cleanup csv to duckdb jl script for docs pic (#10)

--- scripts/chicago-crimes-duckdb.jl
+++ scripts/chicago-crimes-duckdb.jl
@@ -7,15 +7,11 @@ db = DBInterface.connect(DuckDB.DB, ":memory:")
 # import raw CSV crimes data into in-memory DuckDB instance
 println("Loading CSV data ...")
 startTime = Dates.Time(Dates.now())
-DBInterface.execute(db, "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('./data/crimes.csv')")
+DBInterface.execute(db,
+  "CREATE TABLE crimes AS SELECT * FROM read_csv_auto('./data/crimes.csv')")
 endTime = Dates.Time(Dates.now())
 loadTime = round((endTime - startTime), Dates.Second)
 println("CSV data loaded in $(loadTime)")
-
-# print crimes db info schema
-println("\nNew DuckDB info schema:")
-dataTables = DBInterface.execute(db, "SELECT * from information_schema.tables;")
-println(dataTables)
 
 # get crimes table info
 println("\ncrimes table info:")
@@ -26,6 +22,5 @@ println(crimesInfo)
 println("\nDropping crimes table ...")
 DBInterface.execute(db, "DROP TABLE crimes;")
 
-println("Empty DuckDB 'information_schema.tablest' schema:")
 dataTables = DBInterface.execute(db, "SELECT * from information_schema.tables;")
-println(dataTables)
+print("Empty DuckDB 'information_schema.tables' view: $(dataTables)")
